d5bc23fd49c16dbdf413adcb367be3bc0ab8b761
Atta Hasnain <attaulhasnainmaqsoodi@gmail.com>
2023-09-23 07:43:04 +0500

    complete

--- Program 2 Add two numbers/script.mjs
+++ Program 2 Add two numbers/script.mjs
@@ -1,3 +1,5 @@
+// static numbers
+ 
 let a = 3;
 let b = 7;
 let c = 8;
@@ -6,3 +8,15 @@ let c = 8;
 const sum = a + b + c;
 console.log(`the sum of a, b, and c is ${sum}`);
 
+
+
+// dynamic numbers
+
+let a2 = prompt("please enter your first number", "");
+let b2 = prompt("please enter your second number", "");
+let c3 = prompt("please enter your third number", "")
+
+const sum2 = parseInt(a2) + parseInt(b2) + parseInt(c3);
+
+
+console.log(`the sum of a, b, and c is ${sum2}`);
